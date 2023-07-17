dfdfd79d8c96942d634b1b46f677ea3cf1943860
DegefeSam <samuel.degefe7@gmail.com>
2023-07-17 02:47:13 -0700

    Commiting my 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - entry point
  *
- * return: zero(0) when successfull
+ * Return: 0 when successfull
  */
-int main(void) /* This is main function */
+int main(void) /* This is main function with void return */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
