a62f7ff06c600aab9965ed6ad24803c4cbe514c5
DegefeSam <samuel.degefe7@gmail.com>
2023-07-17 01:25:49 -0700

    Commiting my 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  * main Entry point
- *
  * Return zero 
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
