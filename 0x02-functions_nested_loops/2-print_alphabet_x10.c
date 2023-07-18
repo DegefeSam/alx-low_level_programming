452f17ce9d797e669a924289acffb67c6af061bb
DegefeSam <samuel.degefe7@gmail.com>
2023-07-18 13:08:08 -0700

    Commiting my 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,10 +10,10 @@ void print_alphabet_x10(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-       	 for (alpha = 'a'; alpha <= 'z'; alpha++)
-	 {
-		 _putchar(alpha);
-       	 }
- 	 _putchar('\n');
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+		       	_putchar(alpha);
+		}
+		_putchar('\n');
 	}
 }
