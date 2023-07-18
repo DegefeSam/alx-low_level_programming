9758401d8a396ac06200dca8d0cf7fe883e99a9f
DegefeSam <samuel.degefe7@gmail.com>
2023-07-18 11:59:02 -0700

    Commiting my 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 #include <unistd.h>
 /**
- *print_alphabet -Entry point
+ * print_alphabet -Entry point
  *
- *Return: Always 0
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
-	for (char alpha='a'; alpha <= 'z'; alpha++)
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
