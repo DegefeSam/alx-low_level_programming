ad05a8505eeaacc52fdb999c28154c535cec8df5
DegefeSam <samuel.degefe7@gmail.com>
2023-07-18 16:00:26 -0700

    Commiting my 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - find  absolute value of n.
+ * @n: an argument of the function.
+ *
+ * Return: Absolute value n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
