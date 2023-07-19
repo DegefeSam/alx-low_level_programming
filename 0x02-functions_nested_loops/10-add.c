0e1199404b54dba7b21c6a94f444518dbe3bf9e0
DegefeSam <samuel.degefe7@gmail.com>
2023-07-18 17:34:09 -0700

    Commiting my 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -adds two numbers x and y.
+ * @num1: first integer.
+ * @num2: second integer*
+ * Return: final result.
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
