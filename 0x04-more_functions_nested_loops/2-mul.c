e0da952ad33c18b5586484e82eff6061bb1335c7
DegefeSam <samuel.degefe7@gmail.com>
2023-07-20 10:00:54 -0700

    Commiting my 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - entry point of the function to multiply two numbers
+ * Return: always 0 or multiple
+ *@x: first number
+ *@y: second number
+ */
+int mul(int x, int y)
+{
+	int pro;
+
+	pro = x * y;
+	return (pro);
+}
