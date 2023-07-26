35f6c76cf0395a669578e41a02afd794425b74a7
DegefeSam <samuel.degefe7@gmail.com>
2023-07-26 14:18:30 -0700

    Commiting my 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *_strcat - concatenates two strings.
+ *_strncat - concatenates two strings.
  *@src: pointer to source
  *@dest: pointer to destination
  *@n : bytes of src
  *Return: returns dest
  */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src)
 {
 	int a = 0, b;
 
@@ -18,7 +18,7 @@ char *_strcat(char *dest, char *src)
 	}
 	b = 0;
 
-	while (src[b] && b < n != '\0')
+	while (b < n && src[b] != '\0')
 	{
 		dest[a + b] = src[b];
 		b++;
